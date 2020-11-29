a690859758090ae1245742aac87414ce5f4bd5d9
GiraffeSummer <gidoselten@hotmail.com>
2020-11-29 12:41:31 +0100

    more testing

--- index.js
+++ index.js
@@ -109,7 +109,9 @@ function UpdateGrid(x, y, c) {
     })
     if (index > -1) {
         grid[index].color = c;
-        pixels.findOneAndUpdate({ "x": x, "y": y }, { $set: { "color": c } }).then((ab) => { })
+        pixels.findOneAndUpdate({ "x": x, "y": y }, { $set: { "color": c } }).then((ab) => {
+            console.log(ab);
+         })
     }
     return (index > -1)
 }
