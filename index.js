8cd5c56b104a1bbe9646b8afef8c2be8573f3c1a
GiraffeSummer <gidoselten@hotmail.com>
2020-11-29 01:15:05 +0100

    logging index

--- index.js
+++ index.js
@@ -107,7 +107,7 @@ function UpdateGrid(x, y, c) {
     let index = grid.findIndex(g => {
         if (g.x == x && g.y == y) return g
     })
-    console.log("trying update")
+    console.log("trying update " + index)
     if (index > -1) {
         grid[index].color = c;
         pixels.findOneAndUpdate({ x: x, y: y }, { $set: { color: c } }).then((updatedDoc) => {
